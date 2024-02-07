db7b6998493ed8972f5b58994a035bdd9cf44eae
ToastKing1 <owenspreadbury1@gmail.com>
2024-02-07 11:43:13 -0500

    task 2 checkpoint commit

--- Assets/Week 5/Scripts/Knight.cs
+++ Assets/Week 5/Scripts/Knight.cs
@@ -14,6 +14,7 @@ public class Knight : MonoBehaviour
     public float health;
     public float maxHealth = 5;
     bool isDead;
+    public HealthBar healthbar;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -46,12 +47,13 @@ public class Knight : MonoBehaviour
         if (isDead) return;
         clickingOnSelf = true;
         TakeDamage(1);
+        healthbar.TakeDamage(1);
     }
     private void OnMouseUp()
     {
         clickingOnSelf = false;
     }
-    void TakeDamage(float damage)
+    public void TakeDamage(float damage)
     {
         health -= damage;
         health = Mathf.Clamp(health, 0, maxHealth);
