0972e02728eb2d0b27d67d8f7b7ea47b57381595
ToastKing1 <owenspreadbury1@gmail.com>
2024-02-21 15:47:25 -0500

    Week7-Task4b complete: Goalkeeper moves over time instead of teleporting.

--- Assets/Week 7/Scripts/Goalkeeper Controller.cs
+++ Assets/Week 7/Scripts/Goalkeeper Controller.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Experimental.GraphView;
+using Unity.VisualScripting;
 
 public class GoalkeeperController : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class GoalkeeperController : MonoBehaviour
     public Transform goalControllerTransform;
     float distance;
     float goalLine = 2.5f;
+    float speed = 3;
 
     private void FixedUpdate()
     {
@@ -30,7 +32,7 @@ public class GoalkeeperController : MonoBehaviour
             }
 
 
-            rb.transform.position = direction;
+            rb.transform.position = Vector3.MoveTowards(rb.transform.position, direction, speed * Time.deltaTime);
         }
         
     }
